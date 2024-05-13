a38d3f20eeacc891d63ae2c42bc0f416399c9454
mushtariy321 <mushtariyrixsiboyev5a@gmail.com>
2024-05-13 22:10:51 +0300

    done

---
+++ main.js
@@ -0,0 +1,68 @@
+//1
+
+// let num = 5
+// let nu = 2
+// var res = (num % nu)
+// console.log(`Qolidiq ${res}`);
+
+//2
+// alert(Math.floor(Math.random() * 10))
+
+//3
+// let num = 12.510
+// console.log(Math.floor(num));
+
+//4
+// let Mers = (`Mars IT school`)
+// let resu = ''
+// function Mars(Mars) {
+//     alert(Mers.length)
+// }
+// Mars(resu)
+
+
+//5
+// let rep = (`Mars IT School`)
+// let mars = (rep) => {
+//     console.log(rep.repeat(10));
+// }
+// mars(rep)
+
+
+//6
+
+// let harflar = ["a", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+// let M = `M`
+// let u = `U`
+// let sh = `SH`
+// let t = `T`
+// let a = `A`
+// let r = `R`
+// let i = `I`
+// let y = `Y`
+// console.log(M + u + sh + t + a + r + i + y);
+
+//7
+
+// var yosh = +prompt(`Yoshingizni kiriting!`)
+// if (yosh > 18) {
+//     alert(` Siz balag'ot yoshiga yetgansiz😊`)
+// }
+// else if (yosh === 18){
+//     alert(`Balog'at yoshi muborak😊`)
+// }
+// else{
+//     alert(`siz balog'at yoshiga yetmagansiz🙃`)
+// }
+
+// //8
+// let name = prompt("Ismingizni yozing")
+// let reverseName = name.split("").reverse().join("")
+// console.log(reverseName)
+
+//9
+
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let res = sonlar.filter((v, i, a) => v % 2 == 0)
+// console.log(res);
